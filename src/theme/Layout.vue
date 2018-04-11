229fd88b53e5ed04d5bf0adc82c9cfa9a9c501d7
Thibault <thibault@de-boissieu.fr>
2018-04-11 22:42:16 +0200

    Modularisation + router

--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -1,16 +1,31 @@
 <template>
-    <div>
-      <p>Bienvenue !</p>
-      <a href="http://de-boissieu.fr" target="_blanck">
-        <img src="http://bit.ly/vue-img"/>
-      </a>
+    <div id="app">
+      <app-header></app-header>
+      <section class="main-section section">
+        <div class="container content">
+          <router-view></router-view>
+        </div>
+      </section>
+      <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-  export default {}
+  import AppHeader from './AppHeader.vue'
+  import AppFooter from './AppFooter.vue'
+  export default {
+    components: {
+      'app-header': AppHeader,
+      'app-footer': AppFooter
+    }
+  }
 </script>
 
 <style lang="scss">
   @import '~bulma';
+  $primary: #287ab1;
+
+  .columns {
+    flex-wrap: wrap;
+  }
 </style>
