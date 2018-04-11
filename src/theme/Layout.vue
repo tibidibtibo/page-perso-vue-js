ebf6ada83ea02de1f4357d9635ac10625d930b4a
Thibault DE BOISSIEU <thibault.deboissieu@soprasteria.com>
2018-04-11 16:59:09 +0200

    Add babel + vue template

---
+++ src/theme/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <p>Bienvenue !</p>
+      <a href="http://de-boissieu.fr" target="_blanck">
+        <img src="http://bit.ly/vue-img"/>
+      </a>
+    </div>
+</template>
+
+<script>
+  export default {}
+</script>
